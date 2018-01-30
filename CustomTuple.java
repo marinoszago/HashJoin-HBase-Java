0d53fcafeb1d76fa1bd64e3688002e1e49b24291
Marinos Zagkotsis <marinoszagotsis@hotmail.com>
2018-01-30 20:35:12 +0200

    Create CustomTuple.java

---
+++ CustomTuple.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomTuple {
+    String rowID;
+    String key ;
+
+    CustomTuple(String  v1, String  v2) {
+        rowID = v1;
+        key = v2;
+    }
+}
